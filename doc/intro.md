ae9ee24210db218884e091869e9c63e69718fc97
Andreas Ruscheinski <andreas.ruscheinski@googlemail.com>
2018-08-29 17:25:26 +0200

    new lein project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clj-planner
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clj_planner/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-planner.core-test
+  (:require [clojure.test :refer :all]
+            [clj-planner.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
