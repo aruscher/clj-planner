8999630e68ca9040f756fe37c1412de5db5b3d58
Andreas Ruscheinski <andreas.ruscheinski@googlemail.com>
2018-08-30 08:12:59 +0200

    work on parser

--- src/clj_planner/core.clj
+++ src/clj_planner/core.clj
@@ -1,7 +1,36 @@
 (ns clj-planner.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.string :as str]
+            [clojure.pprint :as pprint]))
 
+(def *test-foo* "(define (domain foo))")
 
+(def *foo* (slurp "src/resources/hanoi-problem.pddl"))
 
+(defn tokenize [str]
+  (remove #(str/blank? %)
+          (str/split (str/replace str #"[\(\)]" " $0 ")
+                     #"\s")))
 
-(def *foo* (slurp "src/resources/hanoi-domain.pddl"))
+(defn parse-word
+  [tokens]
+  {:result (first tokens) :used-tokens 1})
+
+(defn parse-domain-name
+  [tokens]
+  (let [{name :result used-tokens :used-tokens} (parse-word (rest tokens))]
+    {:result {:type :domain :name name} :used-tokens 2}))
+
+(defn parse
+  [tokens result]
+  (let [f-token (first tokens) r-tokens (rest tokens)]
+    (cond
+      (= f-token "(") (parse r-tokens result)
+      (= f-token ")") result
+      (= f-token "define") (parse r-tokens result)
+      (= f-token "domain") (let [{name-result :result used-tokens :used-tokens} (parse-domain-name tokens)]
+                             (parse (drop used-tokens tokens) (merge result name-result)))
+      :else (throw (IllegalArgumentException. (str "cant parse " f-token))))))
+
+(pprint/pprint (tokenize *test-foo*))
+(pprint/pprint (parse (tokenize *test-foo*) {}))
