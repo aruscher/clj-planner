88e89510156c47aec8cc2825ae84e20232d4a1b3
Andreas Ruscheinski <andreas.ruscheinski@googlemail.com>
2018-08-30 09:05:56 +0200

    try macro based parser approach

--- src/clj_planner/core.clj
+++ src/clj_planner/core.clj
@@ -5,32 +5,22 @@
 
 (def *test-foo* "(define (domain foo))")
 
-(def *foo* (slurp "src/resources/hanoi-problem.pddl"))
+(def *foo* (slurp "src/resources/hanoi-domain.pddl"))
 
 (defn tokenize [str]
   (remove #(str/blank? %)
           (str/split (str/replace str #"[\(\)]" " $0 ")
                      #"\s")))
 
-(defn parse-word
-  [tokens]
-  {:result (first tokens) :used-tokens 1})
 
-(defn parse-domain-name
-  [tokens]
-  (let [{name :result used-tokens :used-tokens} (parse-word (rest tokens))]
-    {:result {:type :domain :name name} :used-tokens 2}))
 
-(defn parse
-  [tokens result]
-  (let [f-token (first tokens) r-tokens (rest tokens)]
-    (cond
-      (= f-token "(") (parse r-tokens result)
-      (= f-token ")") result
-      (= f-token "define") (parse r-tokens result)
-      (= f-token "domain") (let [{name-result :result used-tokens :used-tokens} (parse-domain-name tokens)]
-                             (parse (drop used-tokens tokens) (merge result name-result)))
-      :else (throw (IllegalArgumentException. (str "cant parse " f-token))))))
 
-(pprint/pprint (tokenize *test-foo*))
-(pprint/pprint (parse (tokenize *test-foo*) {}))
+(defmacro define
+  [[category name] & body]
+  (cond
+    (= category 'domain)(println "domain")
+    (= category 'problem)(println "problem")
+    :else (println "Cant parse " category)))
+
+
+(eval (read-string *foo*))
